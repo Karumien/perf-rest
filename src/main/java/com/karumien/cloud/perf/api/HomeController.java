30d030bbd7d3e7f7b3b7f8e6f91dea39abc91f86
Miroslav Svoboda <miroslav.svoboda@karumien.com>
2019-08-20 23:20:36 +0200

    QA

--- src/main/java/com/karumien/cloud/perf/api/HomeController.java
+++ src/main/java/com/karumien/cloud/perf/api/HomeController.java
@@ -7,15 +7,12 @@
 package com.karumien.cloud.perf.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.karumien.cloud.perf.service.VersionInfoService;
-
-import net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils;
 
 /**
  * Home redirection to swagger api documentation.
